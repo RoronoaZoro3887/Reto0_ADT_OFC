64be90ae443ce398feb578142c96e70964e5fcd8
Aritz <aritzerandio@gmail.com>
2022-09-26 10:07:14 +0200

    Haciendo las conexiones

--- src/model/ImplementBD.java
+++ src/model/ImplementBD.java
@@ -5,30 +5,213 @@
  */
 package model;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author 2dam
  */
-public class ImplementBD implements InterfaceDAO{
+public class ImplementBD implements InterfaceDAO {
 
+    /**
+     * Consulas BD
+     */
+    private final String createClient = "";
+    private final String getDataClient = "";
+    private final String getAccountClient = "";
+    private final String makeAccountClient = "";
+    private final String addClientAccount = "";
+    private final String getDateAccount = "";
+    private final String makeMovements = "";
+    private final String getMovementAccount = "";
+
+       
+    /**
+     * Conexión BD
+     */
+    private String driverBD;
+    private String urlBD;
+    private String userBD;
+    private String passwordBD;
+    
+    private Connection con;
+    private PreparedStatement stmt;
+
+    public ImplementBD(){
+     this.driverBD= ResourceBundle.getBundle("controller.config")
+                .getString("driver");
+        this.urlBD= ResourceBundle.getBundle("controller.config")
+                .getString("urlBD");
+        this.userBD= ResourceBundle.getBundle("controller.config")
+                .getString("userBD");
+        this.passwordBD= ResourceBundle.getBundle("controller.config")
+                .getString("passwordBD");
+    }
+    
+    public void openConnection (){
+        try {
+            con= (Connection) DriverManager.getConnection
+        (urlBD, userBD, passwordBD);
+        } catch (SQLException ex) {
+            Logger.getLogger(ImplementBD.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void closeConnection() {
+        if (stmt!= null) {
+            try {
+                stmt.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ImplementBD.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
+        }
+        if (con!= null) {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ImplementBD.class.getName())
+                        .log(Level.SEVERE, null, ex);
+            }
+        }
+    }
     @Override
     public void createClient(Client cli) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      
+        this.openConnection();
+        
+        try {
+            stmt = (PreparedStatement) con.prepareStatement(createClient);
+            
+            stmt.setString(1, cli.getCity());
+            stmt.setString(2, cli.getEmail());
+            stmt.setString(3, cli.getFirstName());
+            stmt.setString(4, cli.getLastName());
+            stmt.setString(5, cli.getMiddleIntial());
+            stmt.setInt(6, cli.getPhone());
+            stmt.setString(7, cli.getState());
+            stmt.setString(8, cli.getStreet());
+            stmt.setInt(9, cli.getZip());
+            
+            stmt.executeUpdate();
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ImplementBD.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } finally{
+            this.closeConnection();
+        }
     }
 
     @Override
     public Client getDataClient(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ResultSet rs= null;
+        Client cli = null;
+        this.openConnection();
+        
+        try {
+            stmt= (PreparedStatement) con.prepareStatement(getDataClient);
+            stmt.setInt(1, id);
+            
+            rs= stmt.executeQuery();
+            
+            while (rs.next()) {
+                cli = new Client();
+
+                cli.setId(id);
+                cli.setCity(rs.getString("city"));
+                cli.setEmail(rs.getString("email"));
+                cli.setFirstName(rs.getString("firstName"));
+                cli.setLastName(rs.getString("lastName"));
+                cli.setMiddleIntial(rs.getString("middleInitial"));
+                cli.setPhone(rs.getInt("phone"));
+                cli.setState(rs.getString("state"));
+                cli.setStreet(rs.getString("street"));
+                cli.setZip(rs.getInt("state"));
+                
+                
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(ImplementBD.class.getName())
+                            .log(Level.SEVERE, null, ex);
+        } finally{
+            if (rs!= null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ImplementBD.class.getName())
+                            .log(Level.SEVERE, null, ex);
+                }
+            }
+                this.closeConnection();
+        }
+        return cli;
     }
 
     @Override
     public Account getAccountClient(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ResultSet rs= null;
+        Account ac = null;
+        this.openConnection();
+        
+        try {
+            stmt= (PreparedStatement) con.prepareStatement(getDataClient);
+            stmt.setInt(1, id);
+            
+            rs= stmt.executeQuery();
+            
+            while (rs.next()) {
+                ac = new Account();
+
+            
+                
+                
+            }
+           
+        } catch (SQLException ex) {
+            Logger.getLogger(ImplementBD.class.getName())
+                            .log(Level.SEVERE, null, ex);
+        } finally{
+            if (rs!= null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    Logger.getLogger(ImplementBD.class.getName())
+                            .log(Level.SEVERE, null, ex);
+                }
+            }
+                this.closeConnection();
+        }
+        return ac;
     }
 
     @Override
     public void makeAccountClient(Integer id, Account ac) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       this.openConnection();
+        
+        try {
+            stmt = (PreparedStatement) con.prepareStatement(makeAccountClient);
+            
+       
+            stmt.executeUpdate();
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ImplementBD.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } finally{
+            this.closeConnection();
+        }
     }
 
     @Override
