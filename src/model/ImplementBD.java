16ef311b27e9ee926529d677036f38e38b612e80
2dam <2dam@HZ375745.TartangaLH.eus>
2022-09-27 10:09:26 +0200

    Sentencias SQL realizadas

--- src/model/ImplementBD.java
+++ src/model/ImplementBD.java
@@ -25,11 +25,15 @@ public class ImplementBD implements InterfaceDAO{
     
     private final String createClient = "INSERT INTO customer(city, email, firstName, lastName, middleInitial, phone, state, street, zip) VALUES(?,?,?,?,?,?,?,?,?)";
     private final String getDataClient = "SELECT * FROM customer WHERE id = ?";
-    private final String getAccountClient ="SELECT * FROM Account WHERE id = (SELECT id FORM customer WHERE id = ?)";
+    private final String getAccountClient ="SELECT * FROM Account WHERE id = (SELECT accounts_id FORM customer_account WHERE customer_id = ?)";
     private final String makeAccountClient ="INSERT INTO Account(balance, beginBalance, beginBalanceTimestamp, creditLine, description, type) VALUES(?,?,?,?,?,?)";
     private final String addClientAccount = "INSERT INTO customer_account(customers_id, accounts_id) VALUES(?,?)";  
     private final String getDateAccount ="SELECT * FROM account WHERE id = ?";
-    private final String makeMovements = "INSERT INTO movement()";
+    private final String makeMovements = "INSERT INTO movement(amount, balance, description, account_id) VALUES(?,?,?, (SELECT id FROM account WHERE id = ?))";
+    private final String makeMovementsRest = "UPDATE movement SET balance = balance - amount WHERE id = (SELECT id FROM account WHERE id = ?)";
+    private final String makeMovementsPlus = "UPDATE movement SET balance = balance + amount WHERE id = (SELECT id FROM account WHERE id = ?)";
+    private final String getMovementAccount = "SELECT * FROM movement WHERE account_id = (SELECT id FROM account WHERE id = ?)";
+    
     @Override
     public void createClient(Client cli) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
